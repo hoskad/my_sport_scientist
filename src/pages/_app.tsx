6ee61d69c4975d72bee990d37dc3d281189c44c3
hoskad <hoskins.adrian@gmail.com>
2023-05-03 17:25:25 -0400

    Added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { api } from "myss/utils/api";
 
 import "myss/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />;
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
